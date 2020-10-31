12db96ad0955abe8e67688fe1068ef3182d1680b
Devidas Godse <devidaskgodse@gmail.com>
2020-10-31 20:42:18 +0530

    made scriptable parameters

--- functions.py
+++ functions.py
@@ -2,28 +2,29 @@
 # author: Devidas Kachru Godse
 
 import numpy as np
+from sys import argv
 #import networkx
 
+script, no_of_panels, no_of_batteries, demand = argv
 
 # input variables (some could be inserted as arguments)
 ## solar PV parameters
-panel_market_rating = 250 # watts
-cost_per_panel =  0 # Rs. per panel (for the rated one)
+# panel_market_rating = 250 # watts
+# cost_per_panel =  0 # Rs. per panel (for the rated one)
 panel_area = 0.25 # m^2
 panel_efficiency = 0.17
 # need to estimate no. of panels & price
 
 ## battery & inverter parameters
-inverter_cost = 0# based on ratings
+# inverter_cost = 0# based on ratings
 inverter_input_efficiency = 0.9
 inverter_output_efficiency = 0.85
-
 
 battery_voltage_rating = 2# volts
 battery_charge_rating = 100 # Ah
 energy_per_battery = battery_charge_rating * battery_voltage_rating / 1000 # kWh
 battery_DOD = 0.5 # (0.5 for Lead Acid 0.9 for Li-ion)
-battery_cost = 0 # Rs. per battery
+# battery_cost = 0 # Rs. per battery
 # need to estimate no. of batteries & price
 
 
@@ -77,29 +78,26 @@ def IT(phi, longitude, lsm, Ig, Id, n, LST, gamma, beta):
 # daily radiation on a tilted panel
 def HT():
 	pass
-
 '''
 
 ## PV generation based on radiation and amount of panels available
 
 def generate_PV(panel_area, radiation, efficiency):
-	generatedPV = panel_area * radiation * efficiency * 0.75 # 0.75 isto account for all PV losses
+	generatedPV = panel_area * radiation * efficiency * 0.75 # 0.75 is to account for all PV losses
 	return generatedPV
 
-def grid_loss(): # based on wheeling charge calculations
-	return 0.1
+def grid_loss(): 
+	return 0.2
 
-
-# figure out the graph network energy flow
 
 # simulate 
 ## keep the demand constant throughout the day for now
 
-no_of_panels = 20
-no_of_batteries = 10
-charge_capacity = energy_per_battery * no_of_batteries * battery_DOD 
+no_of_panels = float(no_of_panels)
+no_of_batteries = float(no_of_batteries)
+charge_capacity = energy_per_battery * no_of_batteries * battery_DOD
 
-system_cost = battery_cost * no_of_batteries + cost_per_panel * no_of_panels + inverter_cost
+# system_cost = battery_cost * no_of_batteries + cost_per_panel * no_of_panels + inverter_cost
 
 total_PVgeneration = 0
 current_charge = 0 # track level of charge in battery (shouldnot exceed charge capacity)
@@ -109,8 +107,9 @@ grid_energy_losses = 0 # track losses occuring from grid
 # radiation = [4.53, 5.31, 6.12, 6.59, 6.66, 5.73, 4.48, 4.19, 4.96, 5.21, 4.72, 4.16]
 i = 0
 vector = np.zeros([12,6])
-for radiation in [4.53, 5.31, 6.12, 6.59, 6.66, 5.73, 4.48, 4.19, 4.96, 5.21, 4.72, 4.16]: # radiation in kWh/m^2/day
-	current_demand = 10 # kWh
+# for radiation in [4.53*31, 5.31*28, 6.12*31, 6.59*30, 6.66*31, 5.73*30, 4.48*31, 4.19*31, 4.96*30, 5.21*31, 4.72*30, 4.16*31]: # radiation in kWh/m^2/day
+for radiation in [4.53, 5.31, 6.12, 6.59, 6.66, 5.73, 4.48, 4.19, 4.96, 5.21, 4.72, 4.16]:
+	current_demand = int(demand) # kWh
 	Radiation_per_day = 750 #kWh/day #IT()
 	generatedPV = generate_PV(panel_area * no_of_panels, radiation, panel_efficiency)
 	total_PVgeneration += generatedPV
@@ -141,7 +140,7 @@ for radiation in [4.53, 5.31, 6.12, 6.59, 6.66, 5.73, 4.48, 4.19, 4.96, 5.21, 4.
 	i += 1
 
 
-print(vector)
+print(vector[-1,:])
 
 
 '''
